31f469ebb52159fb44efe759284c2e736ccdaf4b
David Pinching <david.pinching@oup.com>
2018-07-24 23:37:38 +0100

    Add ESLint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './index.css';
 
 const app = {};
 
-app.doubled = (x) => x * 2;
+app.doubled = x => x * 2;
 
 console.log('2x2', app.doubled(2));
 
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,13 @@ const config = {
   // Loaders
   module: {
     rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader'
+      },
+      
       // JavaScript/JSX Files
       {
         test: /\.js$/,
